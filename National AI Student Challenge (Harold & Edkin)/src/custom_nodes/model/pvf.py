74729dea6b0139126e98107172753836375e8100
haroldwee <96066817+haroldwee@users.noreply.github.com>
2023-02-06 23:17:25 +0800

    Update pvf.py

--- National AI Student Challenge (Harold & Edkin)/src/custom_nodes/model/pvf.py
+++ National AI Student Challenge (Harold & Edkin)/src/custom_nodes/model/pvf.py
@@ -1,36 +1,40 @@
-"""
-Node template for creating custom nodes.
-"""
-
 from typing import Any, Dict
 
-from peekingduck.pipeline.nodes.abstract_node import AbstractNode
+import cv2
+import numpy as np
+import tensorflow as tf
+
+from peekingduck.pipeline.nodes.node import AbstractNode
+
+IMG_HEIGHT = 180
+IMG_WIDTH = 180
 
 
 class Node(AbstractNode):
-    """This is a template class of how to write a node for PeekingDuck.
+   """Initializes and uses a CNN to predict if an image frame shows a normal
+   or defective casting.   """
 
-    Args:
-        config (:obj:`Dict[str, Any]` | :obj:`None`): Node configuration.
-    """
+   def _init_(self, config: Dict[str, Any] = None, **kwargs: Any) -> None:
+      super()._init(config, node_path=name_, **kwargs)
+      self.model = tf.keras.models.load_model(self.weights_parent_dir)
 
-    def __init__(self, config: Dict[str, Any] = None, **kwargs: Any) -> None:
-        super().__init__(config, node_path=__name__, **kwargs)
+   def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:
+      """Reads the image input and returns the predicted class label and
+      confidence score.
 
-        # initialize/load any configs and models here
-        # configs can be called by self.<config_name> e.g. self.filepath
-        # self.logger.info(f"model loaded with configs: config")
+      Args:
+            inputs (dict): Dictionary with key "img".
 
-    def run(self, inputs: Dict[str, Any]) -> Dict[str, Any]:  # type: ignore
-        """This node does ___.
+      Returns:
+            outputs (dict): Dictionary with keys "pred_label" and "pred_score".
+      """
+      img = cv2.cvtColor(inputs["img"], cv2.COLOR_BGR2RGB)
+      img = cv2.resize(img, (IMG_WIDTH, IMG_HEIGHT))
+      img = np.expand_dims(img, axis=0)
+      predictions = self.model.predict(img)
+      score = tf.nn.softmax(predictions[0])
 
-        Args:
-            inputs (dict): Dictionary with keys "__", "__".
-
-        Returns:
-            outputs (dict): Dictionary with keys "__".
-        """
-
-        # result = do_something(inputs["in1"], inputs["in2"])
-        # outputs = {"out1": result}
-        # return outputs
+      return {
+            "pred_label": self.class_label_map[np.argmax(score)],
+            "pred_score": 100.0 * np.max(score),
+      }
